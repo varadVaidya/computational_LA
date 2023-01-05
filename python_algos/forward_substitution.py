3ed45915d876de6107711c47a0f2c107d6ab02db
Varad Vaidya <vaidyavarad2001@gmail.com>
2023-01-05 11:03:54 +0530

    working for_sub python

--- python_algos/forward_substitution.py
+++ python_algos/forward_substitution.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def forward_substitution(L, b):
     # assumes L as a lower triangluar matrix.
-    if min(abs(np.diag(U))) < 1e-6:
+    if min(abs(np.diag(L))) < 1e-6:
         return False
     
     n = len(b)
@@ -11,17 +11,16 @@ def forward_substitution(L, b):
     x[0] = b[0]/L[0,0]
 
     for i in range(1,n):
-        print("row no: ",i)
         x[i] = (b[i] - L[i,0:i].dot(x[0:i]) )/L[i,i]
     
     return x
 
 L = np.array([
-    [],
-    [],
-    []
+    [3,0,0],
+    [2,-1,0],
+    [1,-2,3]
 ])
-b = np.array([])
+b = np.array([6,-2,2])
 
 x = forward_substitution(L,b)
 print("x is \n", x)
