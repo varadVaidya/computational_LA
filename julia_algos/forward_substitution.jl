095326dfb81d90ad7ecaff64062c6e16ad353ac2
Varad Vaidya <vaidyavarad2001@gmail.com>
2023-01-05 11:12:24 +0530

    added forward sub julia

---
+++ julia_algos/forward_substitution.jl
@@ -0,0 +1,25 @@
+using LinearAlgebra
+
+function forwardsub(L,b)
+    # L is lower triangular matrix
+
+    if minimum(abs.(dig(U))) < 1e-6
+        return false
+    end
+
+    n = length(b)
+    x  = Vector{Float64}(undef,n)
+
+    x[1] = b[1]/L[1,1]
+
+    for i = 2:n
+        x[i] = (b[i] - L[i,1:i+1]' * x[1:i+1])/L[i,i]
+    end
+
+    return x
+end
+
+L = [3 0 0;2 -1 0;1 -2 3]
+b = [6,-2,2]
+
+@show x = forwardsub(L,b)
